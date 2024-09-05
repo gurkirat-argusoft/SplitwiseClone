51cc0c1971ddbca70f86bc32646af812cd642d90
Gurkirat Singh <gurkirat.singh@.argusoft.in>
2024-09-05 12:14:55 +0530

    Group dao created with custom add member query

---
+++ src/main/java/com/splitwise/clone/Repositories/GroupDao.java
@@ -0,0 +1,13 @@
+package com.splitwise.clone.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.splitwise.clone.Entities.Group;
+import com.splitwise.clone.Entities.User;
+
+public interface GroupDao extends JpaRepository<Group, Integer> {
+    @Query(value = "Select * from group",nativeQuery = true)
+    public User addMember(int id);
+    
+}
