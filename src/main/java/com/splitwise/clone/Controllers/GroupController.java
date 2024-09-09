c00c07b9029a7f89f2915f7b71f9df551e60b4d0
Gurkirat Singh <gurkirat.singh@.argusoft.in>
2024-09-09 14:12:59 +0530

    group feature completed

--- src/main/java/com/splitwise/clone/Controllers/GroupController.java
+++ src/main/java/com/splitwise/clone/Controllers/GroupController.java
@@ -1,6 +1,5 @@
 package com.splitwise.clone.Controllers;
 
-import java.util.ArrayList;
 import java.util.*;
 
 import org.apache.el.stream.Optional;
@@ -17,13 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.splitwise.clone.Entities.Group;
+import com.splitwise.clone.Repositories.UserDao;
 import com.splitwise.clone.Services.GroupService;
+import com.splitwise.clone.Services.Userservice;
 
 @RestController
 @RequestMapping("/group")
 public class GroupController {
 @Autowired
 GroupService groupService;
+@Autowired 
+UserDao userDao;
 
     @PostMapping("/create")
     public ResponseEntity<Group> createGroup(@RequestBody Group group){
@@ -54,17 +57,30 @@ GroupService groupService;
     }
 
     @PutMapping("/update/{id}")
-    public Group updateGroup(@PathVariable("id") int id ){
-        return new Group();
+    public ResponseEntity<Group> updateGroup(@PathVariable("id") int id ,@RequestBody Group group ){
+        if(groupService.getGroupById(id)!= null) {
+            return new ResponseEntity<>(groupService.updateGroup(id, group),HttpStatus.ACCEPTED);
+           } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+           }
     }
 
     @DeleteMapping("/delete/{id}")
-    public String deleteGroup(@PathVariable("id") int id ){
-        return "Deleted Group";
+    public ResponseEntity<String> deleteGroup(@PathVariable("id") int id ){
+     try{
+            return new ResponseEntity<>(groupService.deleteGroup(id),HttpStatus.GONE);
+        }catch(Exception e){
+        return new ResponseEntity("Server error occured.", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
     }
 
     @PutMapping("/addmember/{groupId}/{userName}")
-    public Group addMember(@PathVariable("groupId") int groupId,@PathVariable("userName") String userName){
-        return groupService.addMember(userName, groupId);
+    public ResponseEntity<Group> addMember(@PathVariable("groupId") int groupId,@PathVariable("userName") String userName){
+        if (groupService.getGroupById(groupId)!=null&& userDao.findByUserName(userName)!=null) {
+            return new ResponseEntity<>(groupService.addMember(userName, groupId),HttpStatus.ACCEPTED);
+        }else{
+            return new ResponseEntity("Server error occured.",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
