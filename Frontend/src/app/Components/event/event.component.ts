55827b9457a9219ba86944a13ea2b8abebf154d8
gurkirat-argusoft <gurkirat.singh@argusoft.in>
2024-10-16 09:55:45 +0530

    event component corrected

--- Frontend/src/app/Components/event/event.component.ts
+++ Frontend/src/app/Components/event/event.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService } from '../../Services/event.service';
-import { User } from '../../Entites/user';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -11,34 +9,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './event.component.html',
   styleUrl: './event.component.css'
 })
-export class EventComponent implements OnInit {
-  events: Event[] = []
-  members:User[]=[]
-  userId:any=localStorage.getItem('loginUser')
-  constructor(private eventService: EventService) { }
-
-  getAllEventsByUser(){
-    this.eventService.getEventsByUser(this.userId).subscribe((data:Event[])=>{
-      this.events=data
-      this.
-    })
-  }
-  getUsersInEvent(eventId:number){
-    this.eventService.getEventMembers(eventId).subscribe(
-      (members: Set<User>) => {
-        const event = Array.from(this.events).find(g => g.eventId === eventId);
-        if (event) {
-          event.members = members;
-        }
-      },
-      (error) => {
-        console.error('Error fetching users in group', error);
-      }
-    );
-    })
-  }
-  ngOnInit(): void {
-      this.getAllEventsByUser();
-
-  }
+export class EventComponent {
+ 
 }
